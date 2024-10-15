9454cb5d792f2a8da9b5520a720403f9974579a6
Hieu05 <hieuhip.05052004gmail.com>
2024-10-15 21:07:16 +0700

    newcodeuyen

---
+++ config.js
@@ -0,0 +1,12 @@
+const nameGirl = 'Bé iu';
+const giftUrl = 'https://web.facebook.com/touyen.nguyen.203204/';
+const eventName = 'Uyên 08-03 vui vẻ';
+const titleCard = 'Tặng Thế Giới Của Bé';
+const contentCard = 'Chúc Thế Giới của Bé 8/3 duii dẻ nhooo. Cảm ơn Thế Giới vì đã ở bên Bé. Hy vọng là chúng ta sẽ mãi luôn như vậy. Bé thương Thế Giới của bé nè';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Hiếu https://www.facebook.com/HieuFF.05/
+const giftImage = 'uyen.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
